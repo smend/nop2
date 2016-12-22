bf6a38838df0e96d2e5cc1a14bece0c864d0f9b3
DESKTOP-C78UGU3\bizi <mend.shlmi@gmail.com>
2016-12-22 18:57:35 +0200

    Add project files.

---
+++ Plugins/Nop.Plugin.DiscountRules.HasOneProduct/RouteProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.DiscountRules.OnePlusOne
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.DiscountRules.OnePlusOne.Configure",
+                 "Plugins/DiscountRulesOnePlusOne/Configure",
+                 new { controller = "DiscountRulesOnePlusOne", action = "Configure" },
+                 new[] { "Nop.Plugin.DiscountRules.OnePlusOne.Controllers" }
+            );
+            routes.MapRoute("Plugin.DiscountRules.OnePlusOne.ProductAddPopup",
+                 "Plugins/DiscountRulesOnePlusOne/ProductAddPopup",
+                 new { controller = "DiscountRulesOnePlusOne", action = "ProductAddPopup" },
+                 new[] { "Nop.Plugin.DiscountRules.OnePlusOne.Controllers" }
+            );
+            routes.MapRoute("Plugin.DiscountRules.OnePlusOne.ProductAddPopupList",
+                 "Plugins/DiscountRulesOnePlusOne/ProductAddPopupList",
+                 new { controller = "DiscountRulesOnePlusOne", action = "ProductAddPopupList" },
+                 new[] { "Nop.Plugin.DiscountRules.OnePlusOne.Controllers" }
+            );
+            routes.MapRoute("Plugin.DiscountRules.OnePlusOne.LoadProductFriendlyNames",
+                 "Plugins/DiscountRulesOnePlusOne/LoadProductFriendlyNames",
+                 new { controller = "DiscountRulesOnePlusOne", action = "LoadProductFriendlyNames" },
+                 new[] { "Nop.Plugin.DiscountRules.OnePlusOne.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
